cd2b75819d62ebc099ceea0390466eb735936705
RedyPutraSembada <redyputra120503@gmail.com>
2023-07-01 22:40:39 +0700

    kedua

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br />
-          Perubahan Pertama
+          Perubahan Pertama kali
         </p>
         <a
           className="App-link"
